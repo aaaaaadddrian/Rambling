3257d06e94cb1ecb32a074ee21e973d97f92e040
aaaaaadddrian <adrianvolpemath@gmail.com>
2025-02-11 18:19:19 -0600

    this may decode encoded strings as long as they arent hard

--- src/Rambles.java
+++ src/Rambles.java
@@ -1,10 +1,54 @@
+import java.util.Stack;
+
 public class Rambles{
 
+    /*
+    O(N^2)
+    \Omega(1)
+     */
 
     public static void main(String[] args) {
 
+        System.out.println(decoder("3[a2[c]]"));
     }
 
+    public static int Hash(String key){
+        int hash = 0;
+        for(int j = 0; j != key.length(); j++){
+            hash = hash ^ key.charAt(j);
+            hash = hash * (j%32);
+        }
+        return hash;
+    }
 
+    public static String decoder(String encodedString){
+        Stack<Integer> stackerton = new Stack<>();
+        Stack<StringBuilder> stackertonDos = new Stack<>();
+        StringBuilder stringerton = new StringBuilder();
+        StringBuilder lastly = new StringBuilder();
+        int count = 0;
+
+        for(char c : encodedString.toCharArray()){
+            if(Character.isDigit(c)){
+                count = count * 10 +(c -'0');
+            } else if(c == '['){
+                stackerton.push(count);
+                count = 0;
+                stackertonDos.push(stringerton);
+                stringerton = new StringBuilder();
+            } else if(c == ']'){
+                int temp = stackerton.pop();
+
+                for(int i = 0; i < temp; i++){
+                    lastly.append(stringerton);
+                }
+                stringerton = new StringBuilder();
+            }else{
+                stringerton.append(c);
+            }
+        }
+
+        return lastly.append(stringerton).toString();
+    }
 
 }
